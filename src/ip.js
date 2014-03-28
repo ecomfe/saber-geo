66cd031a078bb8be870d8c684fd625786e9027de
zfkun <zfkun@msn.com>
2014-03-28 17:56:40 +0800

    comment & optimize

--- src/ip.js
+++ src/ip.js
@@ -7,31 +7,63 @@ define(function( require ) {
 
     var exports = {};
 
+    /**
+     * @const
+     * @type {string}
+     */
     var PREFIX_CALLBACK = '_SJC_';
+
+    /**
+     * 
+     * @const
+     * @type {string}
+     */
     var PREFIX_ID = '_SJS_';
-    var handlers = {};
 
 
+    /**
+     * callback name for jsonp
+     * 
+     * @public
+     * @type {string}
+     */
     exports.callback = 'callback';
 
+    /**
+     * provider url for ip-server
+     * 
+     * @public
+     * @type {string}
+     */
     exports.provider = 'http://hendless.duapp.com';
 
+    /**
+     * search ip info
+     * 
+     * @public
+     * @param {Function} fn success handler
+     */
     exports.find = function ( fn ) {
-        if ( !fn || typeof fn !== 'function') {
-            return;
+        if ( fn && typeof fn === 'function') {
+            jsonp( exports.provider, fn );
         }
-
-        jsonp( this.provider, fn );
     };
 
+    /**
+     * jsonp request maker
+     * 
+     * @inner
+     * @param {string} url request url
+     * @param {Function} fn success handler
+     */
     function jsonp( url, fn ) {
         var now = Date.now().toString( 16 );
         var id = PREFIX_ID + now;
         var key = PREFIX_CALLBACK + now;
         
         window[ key ] = function () {
-            fn.apply( fn, arguments );
             clean( id, key );
+            fn.apply( fn, arguments );
         };
 
         var script = document.createElement( 'script' );
@@ -43,18 +75,25 @@ define(function( require ) {
             + ( url.indexOf( '?' ) < 0 ? '?' : '&' )
             + exports.callback + '=' + key;
 
-        document.getElementsByTagName('head')[0].appendChild( script );
+        document.getElementsByTagName( 'head' )[ 0 ].appendChild( script );
     }
 
+    /**
+     * cleanup JSONP request
+     * 
+     * @inner
+     * @param {string} id script id for JSONP
+     * @param {string} key key of callback handler for JSONP
+     */
     function clean( id, key ) {
-        try {
-            var node = document.getElementById( id );
-            if ( node && node.parentNode ) {
+        var node = document.getElementById( id );
+        if ( node && node.parentNode ) {
+            try {
                 node.parentNode.removeChild( node );
             }
-            delete window[ key ];
+            catch ( e ) {}
         }
-        catch ( e ) {}
+        delete window[ key ];
     }
 
     return exports;
